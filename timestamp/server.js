6681304b86bc5c370c83d7953c953f2f45c8f690
Ertugrul K <ertugrulk@users.noreply.github.com>
2021-01-06 17:15:31 +0000

    timestamp microservice implementation

--- timestamp/server.js
+++ timestamp/server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +21,32 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/timestamp/:timestamp?", function (req, res) {
+  let param = req.params["timestamp"];
+  let date = new Date(); // default to today
+  if (param) {
+    if (!isNaN(param)) { // check if it's a timestamp
+      date = new Date(+param);
+    } else {
+      date = new Date(param);
+    }
+  }
+  if (date instanceof Date && isFinite(date)) {
+    res.json(
+      {
+        "unix": date.valueOf(),
+        "utc": date.toUTCString()
+      }
+    );
+  } else {
+    res.json(
+      { "error": "Invalid Date" }
+    )
+  }
+});
 
 
 // listen for requests :)
